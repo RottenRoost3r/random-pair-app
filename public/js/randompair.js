d85fa87b6ba07592dcdcb3356ab272f9f2b36b10
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-29 10:07:25 -0400

    added spacing between input boxes

--- public/js/randompair.js
+++ public/js/randompair.js
@@ -1,10 +1,11 @@
-function pushy() {
+function pushy(){
     var x = document.createElement("INPUT");
     x.setAttribute("type", "text");
     x.setAttribute("name", "name[]");
     x.setAttribute("placeholder", "Name");
     var duh = document.getElementById("duh");
     duh.appendChild(x);
+    duh.innerHTML = duh.innerHTML + "  ";
 }
 
 function pully(){
