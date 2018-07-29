e680090411e5d45302d4b77b8b4b4b2c556e0439
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-28 22:34:07 -0400

    fixed thousands of bugs, finished css

--- app.rb
+++ app.rb
@@ -6,16 +6,13 @@ enable :sessions
 get '/' do
   name = session[:name]
   result = session[:result] || ""
-  p "FIRST PAGE PARAMS IS #{params}"
   erb :random_pair, locals:{result: result, name: name}
 end
 
 post '/pairs' do
   name = params[:name] || []
   result = pairing(name)
-  p "HEY RIGHT HERE #{params}"
   session[:result] = result
   redirect '/'
-  erb :pairs, locals:{result: result, name: name}
 end
 
